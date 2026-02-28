3fa4e9510d2dec6ae8e19aadf62e175d9adb68f4
romanovskaiaeva <evaromanovskaia@gmail.com>
2026-03-01 01:26:22 +0300

    algTypes for return added

--- src/core/AllAlgorithms.cpp
+++ src/core/AllAlgorithms.cpp
@@ -9,6 +9,27 @@ std::unique_ptr<AllAlgorithmsI> AlgCreation::create(const std::string &algType) 
     if (algType == "BubbleSort") {
         return std::make_unique<BubbleSort>();
     }
+    else if (algType == "SelectionSort") {
+        return std::make_unique<SelectionSort>();
+    }
+    else if (algType == "InsertionSort") {
+        return std::make_unique<InsertionSort>();
+    }
+    else if (algType == "MergeSort") {
+        return std::make_unique<MergeSort>();
+    }
+    else if (algType == "QuickSort") {
+        return std::make_unique<QuickSort>();
+    }
+    else if (algType == "RandomQuickSort") {
+        return std::make_unique<RandomQuickSort>();
+    }
+    else if (algType == "CountingSort") {
+        return std::make_unique<CountingSort>();
+    }
+    else if (algType == "RadixSort") {
+        return std::make_unique<RadixSort>();
+    }
 
     Logger::getInstance().log("Noname algorithm: " + algType, Logger::ERROR);
     throw std::invalid_argument("No such algorithm" + algType);

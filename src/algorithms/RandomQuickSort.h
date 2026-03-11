4250342a8142315b969cefa9a75ccf06c74da223
SofaKhanova <sofakhanova007@gmal.com>
2026-03-11 13:13:30 +0300

    class RAndomQuickSort implementation

--- src/algorithms/RandomQuickSort.h
+++ src/algorithms/RandomQuickSort.h
@@ -1,11 +1,93 @@
 #pragma once
 
-#include "../core/AllAlgorithms.h"
+#include "../core/AllAlgorithmI.h"
+#include "../utilities/Logger.h"
 
-class RandomQuickSort : public AllAlgorithmsI {
+template<class T>
+class RandomQuickSort : public AllAlgorithmsI<T> {
 public:
-    void sort(std::span<int> data,
-             std::optional<std::function<void(std::span<int>, int, int)>> visualizer) override;
+    void sort(std::span<T> data,
+             std::optional<std::function<void(std::span<T>, int, int)>> visualizer) override {
+        {
+    auto& logger = Logger::getInstance();
+    logger.log("Start: RandomQuickSort", Logger::INFO);
+
+    if (data.size() <= 1) {
+        logger.log("End: RandomQuickSort (array size <= 1)", Logger::INFO);
+        return;
+    }
+
+    std::srand(std::time(nullptr));
+
+    std::function<void(int, int)> quickSortRecursive = [&](int low, int high) {
+        if (low >= high) return;
+
+        int randomIndex = low + rand() % (high - low + 1);
+        int pivot = data[randomIndex];
+
+        if (visualizer) {
+            (*visualizer)(data, randomIndex, -1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(50));
+        }
+
+        int temp = data[randomIndex];
+        data[randomIndex] = data[low + (high - low) / 2];
+        data[low + (high - low) / 2] = temp;
+
+        int i = low;
+        int j = high;
+
+        while (i <= j) {
+            while (i <= high && data[i] < pivot) {
+                i++;
+                if (visualizer) {
+                    (*visualizer)(data, i, -1);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+                }
+            }
+
+            while (j >= low && data[j] > pivot) {
+                j--;
+                if (visualizer) {
+                    (*visualizer)(data, -1, j);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(20));
+                }
+            }
+
+            if (i <= j) {
+                int tempSwap = data[i];
+                data[i] = data[j];
+                data[j] = tempSwap;
+
+                if (visualizer) {
+                    (*visualizer)(data, i, j);
+                    std::this_thread::sleep_for(std::chrono::milliseconds(50));
+                }
+
+                i++;
+                j--;
+            }
+        }
+
+        if (visualizer) {
+            (*visualizer)(data, -1, -1);
+            std::this_thread::sleep_for(std::chrono::milliseconds(30));
+        }
+
+        if (low < j) quickSortRecursive(low, j);
+        if (i < high) quickSortRecursive(i, high);
+    };
+
+    quickSortRecursive(0, data.size() - 1);
+
+    if (visualizer) {
+        (*visualizer)(data, -1, -1);
+        std::this_thread::sleep_for(std::chrono::milliseconds(100));
+    }
+
+    logger.log("End: RandomQuickSort", Logger::INFO);
+}
+    }
 
     std::string algType() const override { return "RandomQuickSort"; }
     std::string complexity() const override { return "O(n)"; }
